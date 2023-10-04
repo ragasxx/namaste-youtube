5699fcdad1724be9ebe94b39f53dca4c34342d95
Sagar Bhatia <sagarbhatia722@gmail.com>
2023-10-04 13:35:24 +0530

    built nested comment section

--- src/components/CommentsContainer.jsx
+++ src/components/CommentsContainer.jsx
@@ -1,7 +1,78 @@
 import React from "react";
 
+const commentsData = [
+  {
+    name: "sagar",
+    text: "hello ji",
+    replies: [
+      {
+        name: "aman",
+        text: "theek h ji",
+        replies: [
+          {
+            name: "pawan",
+            text: "or aman kya haal",
+            replies: [],
+          },
+        ],
+      },
+      {
+        name: "isha",
+        text: "bdiaaaaa hu sagar",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "deepak",
+    text: "kya hal h bhaiyo ke",
+    replies: [
+      {
+        name: "sagar",
+        text: "bdia bhaia",
+        replies: [],
+      },
+    ],
+  },
+];
+
+const Commentslist = ({ commentsData }) => {
+  return commentsData.map((comment) => (
+    <div>
+      <Comment data={comment} />
+      <div className="pl-5 border border-l-black ml-5">
+        <Commentslist commentsData={comment.replies} />
+      </div>
+    </div>
+  ));
+};
+
+const Comment = ({ data }) => {
+  const { name, text } = data;
+  return (
+    <div className="flex bg-gray-100 my-4">
+      <img
+        src="https://e7.pngegg.com/pngimages/178/595/png-clipart-user-profile-computer-icons-login-user-avatars-monochrome-black-thumbnail.png"
+        alt="user image"
+        className="h-12 w-12"
+      />
+      <div className="px-3">
+        <p className="font-bold">{name}</p>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
 const CommentsContainer = () => {
-  return <div>CommentsContainer</div>;
+  return (
+    <div className="m-5 p-2">
+      <h1 className="text-2xl font-bold">Comments :</h1>
+      <div className="mt-5">
+        <Commentslist commentsData={commentsData} />
+      </div>
+    </div>
+  );
 };
 
 export default CommentsContainer;
--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
@@ -15,15 +16,18 @@ const WatchPage = () => {
 
   return (
     <div>
-      <iframe
-        width="1000"
-        height="500"
-        src={`https://www.youtube.com/embed/${searchParam.get("v")}`}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+      <div>
+        <iframe
+          width="1000"
+          height="500"
+          src={`https://www.youtube.com/embed/${searchParam.get("v")}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <CommentsContainer />
     </div>
   );
 };
